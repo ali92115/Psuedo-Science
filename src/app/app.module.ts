e35d78b4e1da0f2a7b42d05e09fc99f89e1efff0
Ali Iftakhar <yushuifti@gmail.com>
2020-09-17 17:59:11 -0700

    Added follow for navbar. Navbar react to button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-dropdown/user-dropdown.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-dropdown',
+  templateUrl: './user-dropdown.component.html',
+  styleUrls: ['./user-dropdown.component.css']
+})
+export class UserDropdownComponent implements OnInit {
+
+  firstTitle = "";
+  @Input() public parentData;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
